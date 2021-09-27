93cd6268f7cc0e2301b52e419400074b1a3e603c
Michelle Bloodworth <83024708+SlimBloodworth@users.noreply.github.com>
2021-09-27 12:43:39 -0500

    Add files via upload

---
+++ AnimalOrMammal/AnimalOrMammal.java
@@ -0,0 +1,16 @@
+public class AnimalOrMammal
+{
+	public static void main (String []args)
+	{
+		System.out.println("Animal Or Mammal\n\n");
+		System.out.println(" _");
+		System.out.println("( \\");
+		System.out.println(" ) )");
+		System.out.println("( (  .-''''-.  A.-.A");
+		System.out.println(" \\ \\/        \\/ , , \\");
+		System.out.println("   \\  \\      =;  t  /=");
+		System.out.println("     \\ |''''. ',--'");
+		System.out.println("     / //   | ||");
+		System.out.println("    /_.))   |_.))");
+		}
+	}
